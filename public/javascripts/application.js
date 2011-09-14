a957929bc330a48f46245f6a92db762354f0ec7b
Paul Springett <paul@simpleweb.co.uk>
2011-09-14 18:08:49 +0100

    Days remaining JS and twitter sharing

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -24,12 +24,29 @@ jQuery(document).ready(function() {
 	);
 	
 	$('ul#comments_list li:odd').addClass ('odd');
+
+	if($('p.days-remaining').length) {
+	  var days = $('p.days-remaining').attr('data-days'),
+	      $hundreds = $('p.days-remaining span.hundreds'),
+	      $units = $('p.days-remaining span.units'),
+	      $tens = $('p.days-remaining span.tens');
+	
+  	var matches = days.match(/(\d)/g);
+	
+  	if(matches.length == 3) {
+  	  $hundreds.html(matches[0]);
+  	  $tens.html(matches[1]);
+  	  $units.html(matches[2]);
+  	} else if(matches.length == 2) {
+  	  $tens.html(matches[0]);
+  	  $units.html(matches[1]);
+  	} else if(matches.length == 1) {
+  	  $units.html(matches[0]);
+  	}
+	}
 	
 	
 	// Fancy Box Code
-	
-    
-
 	$(".simple_login").fancybox({
 		'scrolling'		: 'no',
 		'titleShow'		: false,
@@ -37,6 +54,12 @@ jQuery(document).ready(function() {
 		    $(".error").hide();
 		}
 	});
+
+  // Twitter sharing
+  $(".lnkShareTwitter").click(function() {
+    window.open($(this).attr('href'), 'twitter', 'width=550,height=450');
+    return false;
+  });
 
 });
 
